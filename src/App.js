66a17af4e97cea9f884622750a814a08e79bfc2f
Michaela Lehr <michi_lehr@web.de>
2019-10-23 17:24:48 +0200

    MOD make ant design themeable

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 import React from 'react';
-import { DatePicker } from 'antd';
-import 'antd/dist/antd.css';
-import logo from './logo.svg';
+import { Button, Input, Tabs, Typography } from 'antd';
 import './App.css';
+
+const { TabPane } = Tabs;
+const { Text, Title } = Typography;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <DatePicker />
-      </header>
+    <div className="app">
+      <section className="main">
+        <Title>Welcome to Shortcut.io</Title>
+        <Text>Where your bookmarks life.</Text>
+        <Tabs className="tabs" defaultActiveKey="1">
+          <TabPane className="tab" tab="Login" key="1">
+            <Input addonBefore="Email adress" className="input" placeholder="Email address" />
+            <br /><br />
+            <Input.Password addonBefore="Password" className="input" placeholder="Password" />
+            <br /><br />
+            <Button type="primary">Login</Button>
+          </TabPane>
+          <TabPane className="tab" tab="Sign up" key="2">
+            <Input addonBefore="Email adress" className="input" placeholder="Email address" />
+            <br /><br />
+            <Input.Password addonBefore="Password" className="input" placeholder="Password" />
+            <br /><br />
+            <Input.Password addonBefore="Repeat password" className="input" placeholder="Password" />
+            <br /><br />
+            <Button type="primary">Sign up</Button>
+          </TabPane>
+        </Tabs>
+      </section>
+      <section className="aside"></section>
     </div>
   );
 }
