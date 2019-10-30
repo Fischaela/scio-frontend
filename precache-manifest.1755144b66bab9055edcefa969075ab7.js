b32606182563acdf6b357ce579510fe9c3c8d769
Michaela Lehr <michi_lehr@web.de>
2019-10-30 17:31:08 +0100

    Updates

---
+++ precache-manifest.1755144b66bab9055edcefa969075ab7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c00457d41b61f9de67bc3a07609fe66a",
+    "url": "/scio-frontend/index.html"
+  },
+  {
+    "revision": "7f00cf80a462400e1aad",
+    "url": "/scio-frontend/static/css/2.656a76cb.chunk.css"
+  },
+  {
+    "revision": "93454b582751634cbef5",
+    "url": "/scio-frontend/static/css/main.c8983587.chunk.css"
+  },
+  {
+    "revision": "7f00cf80a462400e1aad",
+    "url": "/scio-frontend/static/js/2.4d5942e3.chunk.js"
+  },
+  {
+    "revision": "93454b582751634cbef5",
+    "url": "/scio-frontend/static/js/main.50b1ba84.chunk.js"
+  },
+  {
+    "revision": "b739cadc00d6a70b41ee",
+    "url": "/scio-frontend/static/js/runtime-main.b7cb74cc.js"
+  }
+]);
