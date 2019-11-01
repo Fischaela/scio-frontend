4fde12b81e57d6ea93bac842a8576a1221c234e1
Michaela Lehr <michi_lehr@web.de>
2019-11-01 20:04:23 +0100

    FIX api url

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
-export const API_URL = `http://localhost:8000/api`
+// export const API_URL = `http://localhost:8000/api`
+export const API_URL = `https://shortcut.io/api`
 export const SCIO_LOCALSTORAGE_ID = `SCIO`
