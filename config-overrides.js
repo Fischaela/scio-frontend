d9ce41e814314576495af3a1e3f83b8ab03aad1e
Michaela Lehr <michi_lehr@web.de>
2019-11-01 14:15:53 +0100

    MOD change colors, add path to screenshots

--- config-overrides.js
+++ config-overrides.js
@@ -10,8 +10,22 @@ module.exports = override(
   addLessLoader({
     javascriptEnabled: true,
     modifyVars: {
-      '@primary-color': '#40557c',
-      '@layout-header-background': '#40557c',
+      '@primary-color': '#0c103e',
+      '@layout-body-background': '#ecf1f5',
+      '@layout-header-background': '#0c103e',
     },
   }),
 );
+
+// blue-primary: #0c103e
+// blue-medium: #1d2e5f
+// blue-lighter: #05c4ff
+// blue-lighter: #cce2f2
+// blue-grey: #3a495b
+// blue-grey-light: #93a4bc
+// blue-grey-lighter: #ecf1f5
+// pink: #e3204b
+// red: #cb0813
+// orange: #e34e20
+// yellow: #ffcc01
+// cyan: #23958d
--- src/components/BookmarkCard.js
+++ src/components/BookmarkCard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, Icon, Tag, Typography } from 'antd';
+import { Card, Tag, Typography } from 'antd';
 
 import './BookmarkCard.css';
 
@@ -8,12 +8,16 @@ const { Paragraph } = Typography;
 class BookmarkCard extends Component {
 
   render() {
+    const screenshotUrl = `
+      https://assets.shortcut.io/${this.props.data.screenshot_id.substring(0, 4)}/${this.props.data.screenshot_id.substring(4, 8)}/${this.props.data.screenshot_id}_500.jpg
+    `
+
     return (
       <Card
         onClick={(e) => this.props.handleClick(e, this.props.data)}
         className="card"
         hoverable
-        cover={<div alt="example" className="card__cover" style={this.props.data.screenshot_id ? {backgroundImage: `url(${this.props.data.screenshot_id})`} : {backgroundImage: 'none'}}></div>}
+        cover={<div alt="example" className="card__cover" style={screenshotUrl ? {backgroundImage: `url(${screenshotUrl})`} : {backgroundImage: 'none'}}></div>}
       >
         <p className="card__title">{this.props.data.title}</p>
         <p className="card__description">{this.props.data.description}</p>
